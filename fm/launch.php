dec57c9048d832ea9cbac2d233fe5c66056e3874
Carter Roeser <carter@cdgtech.one>
2020-11-13 10:54:27 -0800

    Update launch.php

--- fm/launch.php
+++ fm/launch.php
@@ -10,28 +10,17 @@ else { $_SESSION["sftpHost"] = base64_encode($config['HESTIA_HOST_ADDRESS']); }
 
 $_SESSION['ftp_cred'] = base64_encode(hwicrypt($_POST['password'], 'e'));
 
-$postvars1 = array('hash' => $vst_apikey, 'user' => $vst_username,'password' => $vst_password,'cmd' => 'v-add-user-sftp-key','arg1' => $username, 'arg2' => '30');
+$postvars = array('hash' => $vst_apikey, 'user' => $vst_username,'password' => $vst_password,'cmd' => 'v-check-user-password','arg1' => $username,'arg2' => $_POST['password'], 'arg3' => $_SERVER['REMOTE_ADDR']);
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $vst_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($postvars));
+$answer = curl_exec($curl);
 
-$curl1 = curl_init();
-curl_setopt($curl1, CURLOPT_URL, $vst_url);
-curl_setopt($curl1, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($curl1, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl1, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl1, CURLOPT_POST, true);
-curl_setopt($curl1, CURLOPT_POSTFIELDS, http_build_query($postvars1));
-curl_exec($curl1);
-
-$local_file = 'private/sessions/'.$_POST['ftp_user'].'_fm_key';
-$server_file = '/.ssh/hst-filemanager-key';
-$ftp_server=base64_decode($_SESSION["sftpHost"]);
-$ftp_user_name=$_POST['ftp_user'];
-$ftp_user_pass=$_POST['password'];
-
-$conn_id = ftp_connect($ftp_server);
-
-$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
-
-if (ftp_get($conn_id, $local_file, $server_file, FTP_BINARY)) {
+if($answer == 0) {
     header("Location: app");
 }
 else {
